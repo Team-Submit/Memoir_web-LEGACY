8bcccfde79025389c5623a4a477ae40436a05f7b
monotype <spair0039@gmail.com>
2022-09-26 11:22:32 +0900

    [FEAT] 입력 검사 기능(임시)

---
+++ src/signup/signup.js
@@ -0,0 +1,4 @@
+const signUpNameInput = document.getElementById("");
+const signUpIdInput = document.getElementById("");
+const signUpPasswordInput = document.getElementById("");
+const signUpPasswordReInput = document.getElementById("");
