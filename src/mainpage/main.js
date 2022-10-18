a5434bb17371616fad5926a7b7c2961b17c74800
강진현 <kangjin627@gmail.com>
2022-10-18 20:20:24 +0900

    [FEAT] 상세보기 페이지이동 기능 추가

--- src/mainpage/main.js
+++ src/mainpage/main.js
@@ -1,7 +1,21 @@
 const loginbtn = document.querySelector(".loginbtn");
+const mainlist = document.querySelectorAll(".one");
 
 loginbtn.addEventListener("click", logingo);
+mainlist.forEach( function(box) {
+    box.addEventListener("click", reviewDetail)
+});
 
 function logingo(){
     location.href = '../login/login.html';
+}
+
+function reviewDetail(){
+    location.href = '../reviewDetail/reviewDetail.html'
+}
+
+axios.get(baseurl,{
+    
+}){
+
 }
