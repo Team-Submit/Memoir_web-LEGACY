63327f5d48ef15de925086fb8e87a0d03f0514cc
한예슬 <yangsooh386@gmail.com>
2022-11-01 20:21:53 +0900

    [FEAT] 상세보기 글자 가져오기 기능 추가

--- src/reviewDetail/reviewDetail.js
+++ src/reviewDetail/reviewDetail.js
@@ -1,6 +1,15 @@
 const reply = document.getElementById("reply");
 const cmnt = document.getElementById("cmnt");
 const feedback = document.querySelector(".feedback");
+const title = document.getElementById("title");
+const userName = document.getElementById("userName");
+const date = document.getElementById("date");
+const goal = document.getElementById("goal");
+const learned = document.getElementById("learned");
+const felt = document.getElementById("felt");
+const nextGoal = document.getElementById("nextGoal");
+const otherName = document.getElementById("otherName");
+const feedbackCnt = document.getElementById("feedbackCnt");
 
 reply.addEventListener("click", button);
 
@@ -60,3 +69,23 @@ function button(event) {
   
   document.getElementById("cmnt").value = "";
 }
+
+//const url = "http://10.156.147.134:8080/memoir/{id}";
+const url = "https://5540845b-638f-44d6-8017-aab2ea42f445.mock.pstmn.io/reviewDetail";
+
+axios.get(url).then(function(result){
+  console.log("통신 결과 ", result);
+  const contents = result.data.contents;
+
+  title.innerText = contents[0].title;
+  userName.innerText = contents[0].nickname;
+  date.innerText = contents[0].date;
+  goal.innerText = contents[0].goal;
+  learned.innerText = contents[0].learned;
+  felt.innerText = contents[0].felt;
+  nextGoal.innerText = contents[0].nextGoal;
+  otherName.innerText = contents[0].commentsPostResponse[0].writer;
+  feedbackCnt.innerText = contents[0].commentsPostResponse[0].comment;
+}).catch(function(error){
+  console.error(error)
+})
