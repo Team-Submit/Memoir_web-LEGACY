ef2b9f06024e36bb7ce052739f705e7aca5d36f3
강진현 <kangjin627@gmail.com>
2022-11-04 17:18:54 +0900

    [FEAT] 메인 페이지 연동 코드 작성

--- src/mainpage/main.js
+++ src/mainpage/main.js
@@ -4,10 +4,9 @@ const mainlist = document.querySelectorAll(".one");
 axios.get('https://5540845b-638f-44d6-8017-aab2ea42f445.mock.pstmn.io/memoir')
 .then(function(result){
     console.log('통신 결과 : ', result);
-    const contents = result.data.contents;
+    const memoirList = result.data.memoirList;
     
-    console.log("")
-    for(i = 0; i < contents.length; i++){
+    for(i = 0; i < memoirList.length; i++){
         const list = document.querySelector(".list");
 
         const li = document.createElement("li");
@@ -20,22 +19,27 @@ axios.get('https://5540845b-638f-44d6-8017-aab2ea42f445.mock.pstmn.io/memoir')
 
         const title = document.createElement("p");
         title.classList.add("title");
-        title.innerText = contents.title;
+        title.innerText = memoirList[i].title;
         di.appendChild(title);
 
         const ct = document.createElement("p");
         ct.classList.add("contents");
-        ct.innerText = contents.content;
+        if(memoirList[i].content.length >= 65){
+            let stringCut = memoirList[i].content.substring(0, 65);
+            ct.innerText = stringCut + "...";
+        }else{
+            ct.innerText = memoirList[i].content;
+        }
         di.appendChild(ct);
 
         const nida = document.createElement("div");
         nida.classList.add("nida");
         di.appendChild(nida);
 
-        const data = document.createElement("p");
-        data.classList.add("data");
-        data.innerText = contents.date;
-        nida.appendChild(data);
+        const date = document.createElement("p");
+        date.classList.add("date");
+        date.innerText = memoirList[i].createdAt;
+        nida.appendChild(date);
 
         const write = document.createElement("div");
         write.classList.add("write");
@@ -43,17 +47,16 @@ axios.get('https://5540845b-638f-44d6-8017-aab2ea42f445.mock.pstmn.io/memoir')
 
         const nickname = document.createElement("p");
         nickname.classList.add("nickname");
-        nickname.innerText = contents.nickname;
+        nickname.innerText = memoirList[i].nickname;
         write.appendChild(nickname);
+
+        li.addEventListener("click", reviewDetail);
     }
+
+    
 })
 .catch(function(error){
     console.error('error 발생 : ', error);
-});
-
-loginbtn.addEventListener("click", logingo);
-mainlist.forEach( function(box) {
-    box.addEventListener("click", reviewDetail)
 });
 
 function logingo(){
