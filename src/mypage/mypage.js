1bd0701708964ebfa7e16ad0a6f05ac9d2ee6746
강진현 <kangjin627@gmail.com>
2022-11-17 16:27:57 +0900

    [FEAT] 마이페이지 기능 구현 완성 및  연동코드 작성 끝

--- src/mypage/mypage.js
+++ src/mypage/mypage.js
@@ -11,101 +11,129 @@ const lastpw = document.getElementById("lastpw");
 const newpw = document.getElementById("newpw");
 const newpwcheck = document.getElementById("newpwcheck");
 const textreg = document.querySelector(".textreg");
-const baseurl = 'http://172.20.10.7:8080';
+axios.defaults.baseURL = 'http://192.168.137.87:8081';
+let token = localStorage.getItem('access_token') || '';
 
 myifm.addEventListener("click", myopen);
 layout.addEventListener("click", myclose);
-save.addEventListener("click", myclose);
+save.addEventListener("click", saveChange);
 pwchange.addEventListener("click", pwopen);
-pwchangebtn.addEventListener("click", pwclose);
+pwchangebtn.addEventListener("click", pwchangego);
 layout.addEventListener("click", pwclose);
 textreg.addEventListener("click", addnewtext);
+logout.addEventListener("click", logoutgo);
 
-
-axios.get('baseurl/users/mypage')
+function tokencheck(){
+    if(token === ''){
+        location.href  = '../login/login.html';
+    }
+    //토큰 확인하는 코드임
+    axios.get('https://5540845b-638f-44d6-8017-aab2ea42f445.mock.pstmn.io/mypage', {
+        Headers: {Authorization: token,},
+    })
 .then(function(result){
     console.log('결과 : ', result);
 
+    const myPagedata = result.data;
+
     const pwname = document.querySelector(".pwnickname");
-    pwname.innerText = result.data.nickname;
+    pwname.innerText = myPagedata.nickname;
 
     const pwid  = document.querySelector(".pwid");
-    pwid.innerText = result.data.userId;
+    pwid.innerText = myPagedata.userId;
 
     const ifmnickname = document.querySelector(".ifmnickname");
     ifmnickname.innerText = result.data.nickname;
     
     const ifmid = document.querySelector(".ifmid");
-    ifmid.innerText = result.data.userId;
+    ifmid.innerText = myPagedata.userId;
     
     const ifmintroduce = document.querySelector(".ifmintroduce");
-    ifmintroduce.innerText = result.data.introduce;
+    ifmintroduce.innerText = myPagedata.introduce;
 
     const namechange = document.getElementById("namechange");
-    namechange.placeholder = result.data.nickname;
+    namechange.placeholder = myPagedata.nickname;
 
     const itrchange = document.getElementById("itrchange");
-    itrchange.placeholder = result.data.introduce;
+    itrchange.placeholder = myPagedata.introduce;
 
 
 
-    let memoirList = result.data.memoirList;
-    for(i = 0; i < result.data.memoirList.length; i++){
-        const list = document.querySelector(".list");
+    let mypagememoirList = myPagedata.memoirList;
+    for(i = 0; i < mypagememoirList.length; i++){
+        
+        if(mypagememoirList.nickname == myPagedata.nickname){
+            const list = document.querySelector(".list");
 
-        const li = document.createElement("li");
-        li.classList.add("one");
-        list.appendChild(li);
-
-        const di = document.createElement("div");
-        di.classList.add("di");
-        li.appendChild(di);
-
-        const title = document.createElement("p");
-        title.classList.add("title");
-        title.innerText = memoirList[i].title;
-        di.appendChild(title);
-
-        const ct = document.createElement("p");
-        ct.classList.add("contents");
-        if(memoirList[i].content.length >= 65){
-            let stringCut = memoirList[i].content.substring(0, 65);
-            ct.innerText = stringCut + "...";
-        }else{
-            ct.innerText = memoirList[i].content;
+            const li = document.createElement("li");
+            li.classList.add("one");
+            list.appendChild(li);
+            li.id = mypagememoirList[i].id;
+            li.addEventListener("click", idOnclick);
+            function idOnclick(){
+                reviewDetail(li.id);
+            }
+    
+            const di = document.createElement("div");
+            di.classList.add("di");
+            li.appendChild(di);
+    
+            const title = document.createElement("p");
+            title.classList.add("title");
+            title.innerText = mypagememoirList[i].title;
+            di.appendChild(title);
+    
+            const ct = document.createElement("p");
+            ct.classList.add("contents");
+            if(mypagememoirList[i].content.length >= 65){
+                let stringCut = mypagememoirList[i].content.substring(0, 65);
+                ct.innerText = stringCut + "...";
+            }else{
+                ct.innerText = mypagememoirList[i].content;
+            }
+            di.appendChild(ct);
+    
+            const nida = document.createElement("div");
+            nida.classList.add("nida");
+            di.appendChild(nida);
+    
+            const date = document.createElement("p");
+            date.classList.add("date");
+            date.innerText = mypagememoirList[i].createdAt;
+            nida.appendChild(date);
+    
+            const write = document.createElement("div");
+            write.classList.add("write");
+            nida.appendChild(write);
+    
+            const nickname = document.createElement("p");
+            nickname.classList.add("nickname");
+            nickname.innerText = mypagememoirList[i].nickname;
+            write.appendChild(nickname);
+    
+            li.addEventListener("click", reviewDetail);
         }
-        di.appendChild(ct);
-
-        const nida = document.createElement("div");
-        nida.classList.add("nida");
-        di.appendChild(nida);
-
-        const date = document.createElement("p");
-        date.classList.add("date");
-        date.innerText = memoirList[i].createdAt;
-        nida.appendChild(date);
-
-        const write = document.createElement("div");
-        write.classList.add("write");
-        nida.appendChild(write);
-
-        const nickname = document.createElement("p");
-        nickname.classList.add("nickname");
-        nickname.innerText = memoirList[i].nickname;
-        write.appendChild(nickname);
-
-        li.addEventListener("click", reviewDetail);
     }
+        
 })
 .catch(function(error){
     console.error('error 발생 : ', error);
 });
-function reviewDetail(){
+}
+
+
+function reviewDetail(idvalue){
+    localStorage.setItem("PageId", idvalue);
     location.href = '../reviewDetail/reviewDetail.html';
 }
 
 function addnewtext(){
     location.href = '../registration/registration.html';
+}
+
+function logoutgo(){
+    localStorage.removeItem("access_token");
+    location.href = '../mainpage/main.html';
 }
 
 function myopen(){
@@ -118,6 +146,21 @@ function myclose(){
     layout.style.display = 'none';
     myifmodal.style.display = 'none';
 }
+function saveChange(){
+    axios.patch("/users/mypage", {
+        "nickName": namechange.value,
+	    "introduce": itrchange.value
+    }, {
+        Headers: {Authorization: token},
+    })
+    .then(function(result){
+        console.log('통신 결과 : ', result);
+    })
+    .catch(function(error){
+        console.error('error 발생 : ', error);
+    });
+    location.href = '../mypage/mypage.html';
+}
 function pwopen(){
     layout.style.display = 'block';
     pwmodal.style.display = 'block';
@@ -126,11 +169,28 @@ function pwopen(){
     newpwcheck.value = '';
 }
 function pwclose(){
-    if(newpw.value == newpwcheck.value){
-        layout.style.display = 'none';
-        pwmodal.style.display = 'none';
-    }else{
-        alert("새 비밀번호가 일치하지 않습니다.");
-    }
-    
+    layout.style.display = 'none';
+    pwmodal.style.display = 'none';
 }
+
+function pwchangego(){
+
+    if(newpw.value == newpwcheck.value){
+        axios.patch("/users/mypage/update", {
+            "password": newpw.value
+        }, {
+            Headers: {Authorization: token},
+        })
+        .then(function(result){
+            console.log('통신 결과 : ', result);
+        })
+        .catch(function(error){
+            console.error('error 발생 : ', error);
+        });
+        location.href = '../mypage/mypage.html';
+    }else{
+        alert("새비밀번호가 일치하지않습니다.");
+    }
+}
+
+tokencheck();
